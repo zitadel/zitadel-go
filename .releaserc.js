13e4e07cd96fcab18428b85f689d6841740bb3b6
Livio Spring <livio.a@gmail.com>
2022-10-07 11:43:25 +0200

    chore(pipeline): cleanup after branch rename

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,7 @@
 module.exports = {
-    branches: ["main"],
+    branches: [
+        {name: 'v1', range: '0.x.x', channel: '0.x.x'},
+    ],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
