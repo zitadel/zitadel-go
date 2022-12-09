7bf1dd2f4a20d2d2a74feaa7720e9fd42c378b7d
Livio Spring <livio.a@gmail.com>
2022-12-09 14:44:47 +0100

    chore(workflow): fix sem rel plugins to create releases

--- .releaserc.js
+++ .releaserc.js
@@ -4,5 +4,7 @@ module.exports = {
     ],
     plugins: [
         "@semantic-release/commit-analyzer"
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github"
     ]
 };
