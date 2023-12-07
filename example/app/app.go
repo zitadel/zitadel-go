81fbf93f0cce382478b98115f6b39c9d5c5b7ed8
Livio Spring <livio.a@gmail.com>
2023-12-07 10:18:42 +0100

    cleanup

--- example/app/app.go
+++ example/app/app.go
@@ -21,7 +21,7 @@ import (
 var (
 	// flags to be provided for running the example server
 	domain      = flag.String("domain", "", "your ZITADEL instance domain (in the form: https://<instance>.zitadel.cloud or https://<yourdomain>)")
-	key         = flag.String("key", "", "path to your key.json")
+	key         = flag.String("key", "", "encryption key")
 	clientID    = flag.String("clientID", "", "clientID provided by ZITADEL")
 	redirectURI = flag.String("redirectURI", "", "redirectURI registered at ZITADEL")
 	port        = flag.String("port", "8089", "port to run the server on (default is 8089)")
