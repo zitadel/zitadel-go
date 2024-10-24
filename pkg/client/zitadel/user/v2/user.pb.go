7b11885d3b52db716489d40b0d05c47c99feb118
livio-a <9405495+livio-a@users.noreply.github.com>
2024-10-24 06:01:16 +0000

    fix: update zitadel api to v2.64.1

--- pkg/client/zitadel/user/v2/user.pb.go
+++ pkg/client/zitadel/user/v2/user.pb.go
@@ -869,6 +869,8 @@ type SendInviteCode struct {
 
 	// Optionally set a url_template, which will be used in the invite mail sent by ZITADEL to guide the user to your invitation page.
 	// If no template is set, the default ZITADEL url will be used.
+	//
+	// The following placeholders can be used: UserID, OrgID, Code
 	UrlTemplate *string `protobuf:"bytes,1,opt,name=url_template,json=urlTemplate,proto3,oneof" json:"url_template,omitempty"`
 	// Optionally set an application name, which will be used in the invite mail sent by ZITADEL.
 	// If no application name is set, ZITADEL will be used as default.
