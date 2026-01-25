b44a285c57d29870b7832c0830ce337bfae8ca1e
Ilia Choly <ilia.choly@gmail.com>
2026-01-25 16:57:02 +0000

    feat: add CheckAuthorization middleware for optional authorization

--- pkg/http/middleware/authz.go
+++ pkg/http/middleware/authz.go
@@ -36,6 +36,18 @@ func (i *Interceptor[T]) RequireAuthorization(options ...authorization.CheckOpti
 	}
 }
 
+func (i *Interceptor[T]) CheckAuthorization(options ...authorization.CheckOption) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			ctx, err := i.authorizer.CheckAuthorization(req.Context(), req.Header.Get(authorization.HeaderName), options...)
+			if err == nil {
+				req = req.WithContext(authorization.WithAuthContext(req.Context(), ctx))
+			}
+			next.ServeHTTP(w, req)
+		})
+	}
+}
+
 func (i *Interceptor[T]) Context(ctx context.Context) T {
 	return authorization.Context[T](ctx)
 }
